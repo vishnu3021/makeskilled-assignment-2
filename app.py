7016394a1923ae1d9c833d598a01c546c4be5c0c
vishnu3021 <vishnurrddy@gmail.com>
2023-05-13 17:27:26 +0530

    modified hello world  to palindrome string

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print('hello world')
+a='radar'
+b='madhu'
+
+print(type(a),type(b))
+
+#Check - palindrome 
+
+print(a==a[::-1])  # 'radara' == 'radara' - true
+print(b==b[::-1])   # 'madhu' == 'uhdam' - false
